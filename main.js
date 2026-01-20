a7129a8081ed60eb77c3434fe98957a683eb9a58
Sukhman Sidhu <sidhu.sk@obmontessori.edu.ph>
2026-01-20 13:28:05 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function add() {
+ num1 = document.getElementById ('num1').value; 
+ num2 = document.getElementById ('num2').value; 
+ sum = Number(num1) + Number(num2);
+    window.alert(sum);
+}
+
+function subtract() {
+ var num1 = document.getElementById ('num1').value; 
+ var num2 = document.getElementById ('num2').value; 
+ var diff= num1 - num2;
+    window.alert(diff);
+}
